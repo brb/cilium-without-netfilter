28522ec08a226165d6561a7d4cf416ba14610436
Martynas Pumputis <m@lambda.lt>
2019-11-01 11:12:42 +0100

    Update
    Signed-off-by: Martynas Pumputis <m@lambda.lt>

--- bench.sh
+++ bench.sh
@@ -10,7 +10,7 @@ WORKER_IP="$2"
 RESULTS_FILE="$3"
 
 for i in 1 100 1000 2000 2768; do
-    for j in $(seq 1 $i); do sed 's/xxx/$i/g' netperf-svc.yaml | kubectl apply -f -; done
+    for j in $(seq 1 $i); do sed "s/xxx/$i/g" netperf-svc.yaml | kubectl apply -f -; done
     echo "# SVC=$i" >> $RESULTS_FILE
     for j in $(seq 1 3); do
         PORT=$(kubectl get svc | grep "nginx-$((1 + RANDOM % i))" |awk '{print $5}' | cut -d: -f2 | cut -d/ -f1)
